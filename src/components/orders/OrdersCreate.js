4246dfddff83373c3c8fb07cab8c35b64ec6e046
Md Sahi <sahileng2001@gmail.com>
2024-11-29 18:38:20 +0530

    Adding-modal-and-fetch-userid-and-addressid-from-api

--- src/components/orders/OrdersCreate.js
+++ src/components/orders/OrdersCreate.js
@@ -1,6 +1,7 @@
 import { Formik, Field, Form } from 'formik';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 const OrdersCreate = () => {
   const navigate = useNavigate();
@@ -9,6 +10,8 @@ const OrdersCreate = () => {
     order_date: '',
     order_time: '',
     order_status: '',
+    userid: '',
+    isactive: '1',
     paymentamount: '',
     paymentstatus: '',
     paymentmode: '',
@@ -16,24 +19,65 @@ const OrdersCreate = () => {
     delivery_status: '',
     tracking_id: '',
     delivery_partner: '',
+    costamount:'0'
+  };
+  const [showModal, setShowModal] = useState(false);
+  const [submittedValues, setSubmittedValues] = useState(null);
+  const [userIds, setUserIds] = useState([]);
+  const [addressIds, setAddressIds] = useState([]);
+
+  useEffect(() => {
+    const fetchUserIds = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}users/allusers`);
+        setUserIds(response.data);
+      } catch (error) {
+        console.error('Error fetching order IDs:', error);
+      }
+    };
+    const fetchAddressIds = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}address/allAddress`);
+        setAddressIds(response.data);
+      } catch (error) {
+        console.error('Error fetching address IDs:', error);
+      }
+    }
+    fetchUserIds();
+    fetchAddressIds();
+  }, []);
+
+  const handleFormSubmit = (values) => {
+    setShowModal(true);
+    setSubmittedValues(values);
   };
 
-  const submitOrder = async values => {
-    try {
-      const confirmed = window.confirm(
-        'Are you sure you want to Add New Order???'
-      );
-      if (confirmed) {
-        await axios.post(
-          `${process.env.REACT_APP_API_URL}order/addOrder`,
-          values
-        );
-        navigate('/orders');
-      }
+  const confirmSubmission = async () => {
+    try{
+      await axios.post(`${process.env.REACT_APP_API_URL}orders/addOrder`, submittedValues);
+      setShowModal(false);
+      navigate('/orders');
     } catch (error) {
       console.log(error);
     }
   };
+
+  // const submitOrder = async values => {
+  //   try {
+  //     const confirmed = window.confirm(
+  //       'Are you sure you want to Add New Order???'
+  //     );
+  //     if (confirmed) {
+  //       await axios.post(
+  //         `${process.env.REACT_APP_API_URL}orders/addOrder`,
+  //         values
+  //       );
+  //       navigate('/orders');
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   return (
     <>
@@ -41,21 +85,28 @@ const OrdersCreate = () => {
       <Formik
         enableReinitialize={true}
         initialValues={formValues}
-        onSubmit={values => submitOrder(values)}
+        onSubmit={values => handleFormSubmit(values)}
       >
         <Form>
-          <div className='row mb-2'>
-            <label className='col-4 my-2 text-center'>Order_Id:-</label>
-            <Field name='order_id' type='text' className='col-6' />
+          <div className="row mb-2">
+            <label className="col-4 my-2 text-center">User ID:</label>
+            <Field as="select" name="userid" className="col-6">
+              <option value="">Select an User ID</option>
+              {userIds.map((user) => (
+                <option key={user.userid} value={user.userid}>
+                  {user.userid}
+                </option>
+              ))}
+            </Field>
           </div>
-          <div className='row mb-2'>
+          {/* <div className='row mb-2'>
             <label className='col-4 my-2 text-center'>Order_Date:-</label>
             <Field name='order_date' type='text' className='col-6' />
           </div>
           <div className='row mb-2'>
             <label className='col-4 my-2 text-center'>Order_Time:-</label>
             <Field name='order_time' type='text' className='col-6' />
-          </div>
+          </div> */}
           <div className='row mb-2'>
             <label className='col-4 my-2 text-center'>Order_Status:-</label>
             <Field name='order_status' type='text' className='col-6' />
@@ -73,30 +124,70 @@ const OrdersCreate = () => {
             <label className='col-4 my-2 text-center'>PaymentMode:-</label>
             <Field name='paymentmode' type='text' className='col-6' />
           </div>
-          <div className='row mb-2'>
-            <label className='col-4 my-2 text-center'>AddressId:-</label>
-            <Field name='addressid' type='text' className='col-6' />
+          <div className="row mb-2">
+            <label className="col-4 my-2 text-center">Address ID:</label>
+            <Field as="select" name="addressid" className="col-6">
+              <option value="">Select an Address ID</option>
+              {addressIds.map((address) => (
+                <option key={address.addressid} value={address.addressid}>
+                  {address.addressid}
+                </option>
+              ))}
+            </Field>
           </div>
           <div className='row mb-2'>
             <label className='col-4 my-2 text-center'>Delivery_Status:-</label>
             <Field name='delivery_status' type='text' className='col-6' />
           </div>
-          <div className='row mb-2'>
+          {/* <div className='row mb-2'>
             <label className='col-4 my-2 text-center'>Tracking_Id:-</label>
             <Field name='tracking_id' type='text' className='col-6' />
-          </div>
+          </div> */}
           <div className='row mb-2'>
             <label className='col-4 my-2 text-center'>Delivery_Partner:-</label>
             <Field name='delivery_partner' type='text' className='col-6' />
           </div>
           <div className='hey'>
-            <button type='submit'>Submit Now</button>
+            <button type='submit' >Submit Now</button>
             <Link to='/orders' className='btn btn-danger back'>
               Back
             </Link>
           </div>
         </Form>
       </Formik>
+
+      {showModal && ( 
+        <div className='modal fade show' style={{ display: 'block' , backgroundColor: 'rgba(0, 0, 0, 0.5)'}}>
+          <div className='modal-dialog modal-dialog-centered'>
+            <div className='modal-content'>
+              <div className='modal-body'>
+                <p>Are you sure you want to Add New Order?</p>
+              </div>
+              <div className='modal-footer'>
+              <button
+                  type='button'
+                  className='btn btn-primary'
+                  onClick={confirmSubmission}
+                  
+                >
+                  Confirm
+                </button>
+              <button
+                type='button'
+                className='btn btn-secondary'
+                onClick={() => {
+                  setShowModal(false);
+                }}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>  
+      </div>
+
+        
+      )}
     </>
   );
 };
--- src/components/orders/OrdersUpdate.js
+++ src/components/orders/OrdersUpdate.js
@@ -20,6 +20,10 @@ const OrdersUpdate = () => {
     delivery_partner: '',
   });
 
+  const [showNoChnagesModal, setShowNoChnagesModal] = useState(false);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [updateValues, setUpdateValues] = useState({});
+
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_API_URL}orders/getById/${id}`)
@@ -47,37 +51,63 @@ const OrdersUpdate = () => {
   // const handleFieldChange = (e) => {
   //   setFormValues(e.target.value);
   // };
-
-  const updateOrder = async values => {
-    try {
-      // Check if form values are unchanged
-      const isUnchanged = Object.keys(formValues).every(
-        key => formValues[key] === values[key]
-      );
-
-      if (isUnchanged) {
-        alert('No changes were made. Nothing to update.');
-        return;
-      }
-      //after form values update
-      const confirmed = window.confirm(
-        'Are you sure you want to Update this order?'
-      );
-      if (confirmed === true) {
-        await axios.put(
-          `${process.env.REACT_APP_API_URL}order/updateOrder/${id}`,
-          values
-        );
-        navigate('/orders');
-      }
-    } catch (err) {
-      console.log(err);
+  const handleFormSbmit =values => {
+    const isUnchanged = Object.keys(formValues).every(
+      key => formValues[key] === values[key]
+    );
+    if (isUnchanged) {
+      setShowNoChnagesModal(true);
+    } else {
+      setShowUpdateModal(true);
+      setUpdateValues(values);
     }
   };
+  
+  const confirmUpdate = async() => {
+    try{
+      await axios.put(
+        `${process.env.REACT_APP_API_URL}orders/updateOrder/${id}`,
+        updateValues
+      );
+      setShowUpdateModal(false);
+      navigate('/orders');
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+    
 
-  useEffect(() => {
-    console.log('formValues', formValues);
-  }, [formValues]);
+  // const updateOrder = async values => {
+  //   try {
+  //     // Check if form values are unchanged
+  //     const isUnchanged = Object.keys(formValues).every(
+  //       key => formValues[key] === values[key]
+  //     );
+
+  //     if (isUnchanged) {
+  //       alert('No changes were made. Nothing to update.');
+  //       return;
+  //     }
+  //     //after form values update
+  //     const confirmed = window.confirm(
+  //       'Are you sure you want to Update this order?'
+  //     );
+  //     if (confirmed === true) {
+  //       await axios.put(
+  //         `${process.env.REACT_APP_API_URL}order/updateOrder/${id}`,
+  //         values
+  //       );
+  //       navigate('/orders');
+  //     }
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   console.log('formValues', formValues);
+  // }, [formValues]);
 
   return (
     <>
@@ -85,12 +115,12 @@ const OrdersUpdate = () => {
       <Formik
         enableReinitialize={true}
         initialValues={formValues}
-        onSubmit={values => updateOrder(values)}
+        onSubmit={values => handleFormSbmit(values)}
       >
         <Form>
           <div className='row mb-2'>
             <label className='col-4 my-2 text-center'>Order_Id:-</label>
-            <Field name='order_id' type='text' className='col-6' />
+            <Field name='order_id' type='text' className='col-6' readOnly />
           </div>
           <div className='row mb-2'>
             <label className='col-4 my-2 text-center'>Order_Date:-</label>
@@ -119,7 +149,7 @@ const OrdersUpdate = () => {
           </div>
           <div className='row mb-2'>
             <label className='col-4 my-2 text-center'>AddressId:-</label>
-            <Field name='addressid' type='text' className='col-6' />
+            <Field name='addressid' type='text' className='col-6' readOnly />
           </div>
           <div className='row mb-2'>
             <label className='col-4 my-2 text-center'>Delivery_Status:-</label>
@@ -127,7 +157,7 @@ const OrdersUpdate = () => {
           </div>
           <div className='row mb-2'>
             <label className='col-4 my-2 text-center'>Tracking_Id:-</label>
-            <Field name='tracking_id' type='text' className='col-6' />
+            <Field name='tracking_id' type='text' className='col-6' readOnly />
           </div>
           <div className='row mb-2'>
             <label className='col-4 my-2 text-center'>Delivery_Partner:-</label>
@@ -141,6 +171,59 @@ const OrdersUpdate = () => {
           </div>
         </Form>
       </Formik>
+
+      {showNoChnagesModal && (
+        <div className='modal fade show' style={{ display: 'block',backgroundColor:'rgba(0,0,0,0.5)' }}>
+          <div className='modal-dialog modal-dialog-centered'>
+            <div className='modal-content'>
+              <div className='modal-body'>
+                <p>No changes made</p>
+              </div>
+              <div className='modal-footer'>
+                <button
+                  type='button'
+                  className='btn btn-secondary'
+                  data-dismiss='modal'
+                  onClick={() => setShowNoChnagesModal(false)}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {showUpdateModal &&(
+        <div className='modal fade show' style={{display:'block',backgroundColor:'rgba(0,0,0,0.5)'}}>
+          <div className='modal-dialog modal-dialog-centered'>
+            <div className='modal-content'>
+              <div className=' modal-body'>
+                <p>Are you sure you want to update this order?</p>
+              </div>
+              <div className='modal-footer'>
+                <button
+                  type='button'
+                  className='btn btn-primary'
+                  data-dismiss='modal'
+                  onClick={ confirmUpdate}
+                >
+                  Confirm
+                </button>
+                <button
+                  type='button'
+                  className='btn btn-secondary'
+                  data-dismiss='modal'
+                  onClick={()=>setShowUpdateModal(false)}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+            
+      )}
     </>
   );
 };
